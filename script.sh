b6491bd4ea86b02424de4efa43ecf87debf2c775
alirsali <alirsali@gmail.com>
2025-04-16 16:33:40 -0600

    branch to test

--- script.sh
+++ script.sh
@@ -1,23 +1,32 @@
           regexBranchPattern=$(git branch -r | grep -E 'origin/feature-E-[0-9]+-.*' | sed 's|origin/||')
           echo "$rexBranchPattern"
 
+          echo "-----------------------------------------------------------------------------------------------------------------"
+
           
           # Obtener las ramas fusionadas en main
-          MERGED_BRANCHES=$(git branch -a --format "%(refname:short) %(upstream)" | awk '$2 !~/\/origin\// { print $1 }')
+          LISTEDALLBRANCHES=$(git branch -a --format "%(refname:short) %(upstream)" | awk '$2 !~/\/origin\// { print $1 }')
+          echo "-----------------------------------------------------------------------------------------------------------------"
+
+          MERGED_BRANCHES=$(git branch --merged)
+          echo "-----------------------------------------------------------------------------------------------------------------"
+
           
-          sleep 5
           # Filtrar solo las que cumplan con el patrón
           FILTERED_BRANCHES=$(echo "$MERGED_BRANCHES" | grep -E "$regexBranchPatter" || true)
-
+          echo "-----------------------------------------------------------------------------------------------------------------"
+          
+          sleep 5
           if [[ -z "$FILTERED_BRANCHES" ]]; then
             echo "No merged feature branches to delete."
             exit 0
           fi
+          echo "-----------------------------------------------------------------------------------------------------------------"
 
           echo "Deleting merged feature branches:"
           echo "$FILTERED_BRANCHES"
 
-          for BRANCH in $FILTERED_BRANCHES; do
+          for BRANCH in $FILTERED_BRANCHES; do  
             echo "$BRANCH"
-            #git push origin --delete $BRANCH
+            git push origin --delete $BRANCH
           done
