0ae2f6b59d986149d546f9d44e1a03be178e5d70
Babxiluca <87202918+Babxiluca@users.noreply.github.com>
2025-07-30 16:50:43 -0600

    Terraform azure alba (#91)
    * terraform certification exe
    
    * removing the account

--- Azure_TF/provider.tf
+++ Azure_TF/provider.tf
@@ -9,23 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "5156f0c3-0314-436e-a712-5a97fe5cc0f1"
-  alias = "eastus"
-  location = "eastus"
-  skip_provider_registration = true
-  
+  subscription_id =
 }
-
-provider "azurerm" {
-  features {}
-  alias = "westeurope"
-  subscription_id = var.subscription_id
-  skip_provider_registration = true
-  location = "westeurope"
-}
-
-
-
 
 terraform {
   required_version = ">= 1.9.0, < 2.0.0"
